0d07d288b5d9e6b204f973ddf809c00efc223a96
whyneverland <whyneverland@yandex.ru>
2025-07-11 16:27:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const sections = document.querySelectorAll('.section');
 
+const onScroll = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < triggerBottom) {
+      section.classList.add('visible');
+    }
+  });
+};
+
+window.addEventListener('scroll', onScroll);
+window.addEventListener('load', onScroll);
